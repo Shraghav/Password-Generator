01c62cc3746e6d782efcf9aceb8333197da5867b
Shraghav <harishragavan04@gmail.com>
2024-07-07 23:37:25 +0530

    deleted build files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-// import './App.css';
-// import PasswordPostResult from './templates/PassworPostResult';
-// import PasswordGetResult from './templates/PasswordGetResult';
-// import PasswordDeleteResult from './templates/PasswordDeleteResult';
-// import PasswordUserCreate from './templates/PasswordUserCreate';
 import Home from './Router/Home';
 const PasswordGenerator = () => {
   return (
     <div>
-      {/* <PasswordPostResult />
-      <PasswordGetResult />
-      <PasswordDeleteResult />
-      <PasswordUserCreate /> */}
       <Home/>
     </div>
   )
